fa4b8dd5aa9e8b211f09a3545e1dcaf4640603ef
Thomah <5090230+Thomah@users.noreply.github.com>
2019-07-09 14:39:32 +0200

    Display Slack token to debug

--- src/main/java/fr/thomah/rogerserver/RogerServerApplication.java
+++ src/main/java/fr/thomah/rogerserver/RogerServerApplication.java
@@ -24,6 +24,7 @@ public class RogerServerApplication {
 
 		JsonParser jsonParser = new JsonParser();
 		String token = System.getenv("SLACK_TOKEN");
+		System.out.println(token);
 
 		try (RTMClient rtm = new Slack().rtm(token)) {
 
